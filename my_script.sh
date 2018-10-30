5c5f13e88ff5616522ceb3ba8bea075f81cbeefd
PFC-webprojects <E829159@posteo.net>
2018-10-29 21:09:44 -0700

    We nuked the local edition of the repository and started over.

--- my_script.sh
+++ my_script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+echo ""
+
 echo "Hello ITC-136"
 echo "Hello hello!"
+echo ""
 
+echo "This is the current working directory:"
+echo pwd
+echo ""
+
+echo "This is the version of Linux running:"
+echo uname -a
+echo ""
+
+echo "These are the logged-in users:"
+who
+echo ""
 
