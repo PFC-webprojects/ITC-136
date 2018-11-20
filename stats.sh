f1887afbe456e0cd8f5b32006fd4addb73f424ea
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-11-19 20:18:07 -0800

    Improvement to Logged-In Users line
    Modified the use of the who command to display only any users' login names, and no other columns of information.

--- stats.sh
+++ stats.sh
@@ -3,7 +3,7 @@
 diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}' )
 memfree=$( free -mh | grep Mem: | awk '{print $7}' )
 connections=$( netstat | grep tcp )
-users=$( who )
+users=$( who | awk '{print $1}' )
 
 echo "
 
