106b98252f1b42e4f1ed41090128d6390ea7855d
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-11-19 20:02:36 -0800

    Scripting Lab 0
    Work in groups to create a bash script that shows the following when run:
    * how much disk space has been used
    * how much memory is free
    * how many connections there are to the current machine
    * who is logged in

---
+++ stats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}' )
+memfree=$( free -mh | grep Mem: | awk '{print $7}' )
+connections=$( netstat | grep tcp )
+users=$( who )
+
+echo "
+
+This is a snapshot of your current system:
+
+Disk Space Used:           $diskused
+Free Memory:               $memfree
+Logged-In Users:           $users
+Open Internet Connections: $connections
+
+"
